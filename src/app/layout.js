72e220ce58e3fb92c1f890251e8538b5656639bf
codingpalette <codingpalette@gmail.com>
2023-08-14 14:34:23 +0900

    next-ui 적용

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,8 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
+import {NextUiWrapper} from "@/app/components/wrapper/NextUiWrapper";
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Create Next App',
@@ -10,8 +11,12 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ko">
+      <body>
+      <NextUiWrapper>
+        {children}
+      </NextUiWrapper>
+      </body>
     </html>
   )
 }
